bb9b037b45627fb2280332170a1f990ae0bb86e9
Adam <am1macdonald@gmail.com>
2022-02-06 19:04:01 -0500

    adding functions

--- src/classes/dropDown.js
+++ src/classes/dropDown.js
@@ -7,7 +7,39 @@ export class DropDownMenu {
         this.element = domRef
         
         const contain = (() => {
-            console.log(this.element.childNodes)
+            let target = this.element
+            let content = target.firstElementChild
+
+            let button = document.createElement('button')
+            let arrow = document.createElement('div')
+            let hidden = document.createElement('div');
+
+            button.type = 'button'
+            button.innerHTML = 'Menu'
+            button.classList.add('drop-down')
+
+            button.addEventListener('click', () => {
+                hidden.classList.toggle('animate')
+            })
+
+            arrow.classList.add('arrow-down')
+
+            hidden.classList.add('hidden')
+
+            content.remove()
+
+            button.append(arrow)
+
+            hidden.append(content);
+
+            target.append(button)
+            target.append(hidden)
+
+            
+            
+            console.log(content)
+
+
         })()
     }
     slideDown () {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import "./stylesheets/temp.sass"
 import image from "./assets/internet.png"
 import { DropDownMenu } from "./classes/dropDown";
 
-let dropDown = document.getElementById('nav-menu');
+let dropDown = document.getElementById('target');
 
 new DropDownMenu(dropDown);
