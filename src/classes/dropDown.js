2c9c02318042202b6bae43ab69710269006c6c27
Adam <am1macdonald@gmail.com>
2022-02-09 19:18:04 -0500

    finished up styling and refactored the script

--- src/classes/dropDown.js
+++ src/classes/dropDown.js
@@ -8,44 +8,17 @@ export class DropDownMenu {
         
         const contain = (() => {
             let target = this.element
+
             let content = target.firstElementChild
-
-            let button = document.createElement('button')
-            let arrow = document.createElement('div')
-            let hidden = document.createElement('div');
-
-            button.type = 'button'
-            button.innerHTML = 'Menu'
-            button.classList.add('drop-down')
-
-            button.addEventListener('click', () => {
-                hidden.classList.toggle('animate')
-            })
-
-            arrow.classList.add('arrow-down')
-
-            hidden.classList.add('hidden')
-
             content.remove()
-
-            button.append(arrow)
-
-            hidden.append(content);
-
-            target.append(button)
-            target.append(hidden)
-
+            content.classList.add('drop-down')
             
-            
-            console.log(content)
+            let container = document. createElement('div')
+            container.classList.add('drop-down-container')
+            container.innerHTML = 'MENU'
 
-
+            container.appendChild(content)
+            target.append(container)
         })()
-    }
-    slideDown () {
-        let target = this.element
-        target.addEventListener('click', () => {
-            target.classlist.toggle('slide');
-        })
     }
 }
